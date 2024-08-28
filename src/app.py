625087ca44535cbb711728da639a359cb7710694
jjjaeunn <jjjaeunn@gmail.com>
2024-08-29 01:46:27 +0900

    add: 추천 서버 구현

--- src/app.py
+++ src/app.py
@@ -1,20 +1,20 @@
 import joblib
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import get_preference
 
 app = Flask(__name__)
 
-@app.route("/recommend")
+@app.route("/recommend", methods=['GET'])
 def model():
     # input parameter - 입력값 str으로 %20 구분해서 연결한 값
     input = request.args.get('input')
     if input:
         input = ''.join(input.split('%20'))
-        prediction = get_preference.recommend(input)
+        prediction = get_preference.recommend(user_input=input)
         # json 형식으로 변환
     else:
         prediction = {}
-    return prediction
+    return make_response(prediction, 200)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
