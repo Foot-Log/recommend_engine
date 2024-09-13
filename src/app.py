7d73d1032c35d270ac7781c50c892596e9aeab4c
jjjaeunn <jjjaeunn@gmail.com>
2024-09-13 22:06:10 +0900

    fix : request body 반영한 최종최종

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request, make_response, abort
 #     JWTManager, jwt_required, create_access_token, get_jwt_identity, unset_jwt_cookies, create_refresh_token, jwt_refresh_token_required,
 # )
 import get_preference
+import json
 
 app = Flask(__name__)
 
@@ -26,27 +27,28 @@ def root():
 def model():
     # input parameter - 입력값 str으로 %20 구분해서 연결한 값
     prediction = {}
+    app.logger.info("request 이전")
+
     if request.method == "POST":
 #        # 요청 정보 로깅 (f-string 사용)
         app.logger.info(f"Request: Method={request.method}, Path={request.path}, Headers={request.headers}")
-        
-        # JSON payload 로깅
-        payload = request.get_json(silent=True)  # 요청 본문이 JSON 형식일 경우만 로깅
+
+        payload = request.get_json()
         if payload:
             app.logger.info(f"Payload: {payload}")
         else:
             app.logger.info("No JSON payload or unable to parse JSON")
+        input_list = []
+        for item in payload.values():
+            input_list.extend(item)
 
-        input_list = payload['firstKeyword'].extend(payload['secondKeyword']).extend(payload['thirdKeyword'])
         #input = request.args.get('input')
         if input_list:
-            input = ''.join(input_list.split('%20'))
+            input = ' '.join(input_list)
+            print(input)
             prediction = {'course_id':get_preference.recommend(user_input=input)}
 
-        # else:
-        #     prediction = {'course_id': ['2547514', '2547399', '2549270', '2547444', '2547535', '2549163', '2549164', '2549273', '2549170', '2508718']}
-        
     return make_response(prediction, 200)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5050, debug=False)
--- src/get_preference.py
+++ src/get_preference.py
@@ -5,19 +5,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import joblib
 import json
+import os
 
 def recommend(user_input, top_n=10):
     # 코사인 유사도 계산
-
-    tfidfvectorizer = joblib.load('model/vectorizer.pkl')
-    tfidf_matrix = np.load('model/matrix.npy')
+    print()
+    tfidf_matrix = np.load(os.getcwd()+'/src/model/matrix.npy')
+    tfidfvectorizer = joblib.load(os.getcwd()+'/src/model/vectorizer.pkl')
+    
     preference_vector = tfidfvectorizer.transform([user_input]).toarray().tolist()
     sim_scores = cosine_similarity(preference_vector, tfidf_matrix).flatten()
     
     # 유사도 점수를 기준으로 정렬하여 가장 유사한 문서의 인덱스 찾기
     top_indices = np.argsort(sim_scores)[::-1][:top_n].tolist()
 
-    json_file_path = 'data/course.json'
+    json_file_path = os.getcwd() + '/src/data/course.json'
     with open(json_file_path, 'r', encoding='utf-8') as f:
         json_data = json.load(f)
 
