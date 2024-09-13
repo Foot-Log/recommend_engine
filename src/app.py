2322c8b68f0dd8482d9256ef1880d2da3b9bbcb9
jjjaeunn <jjjaeunn@gmail.com>
2024-09-13 20:44:27 +0900

    request logger 추가

--- src/app.py
+++ src/app.py
@@ -43,4 +43,4 @@ def model():
     return make_response(prediction, 200)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
